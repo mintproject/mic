d5f5fb398accdd3d0e174de569bcd0737feb035b
Maximiliano Osorio <maxiosorio@gmail.com>
2020-06-06 10:06:40 -0700

    fix: tests

--- src/mic/tests/test_model_catalog_utils.py
+++ src/mic/tests/test_model_catalog_utils.py
@@ -1,6 +1,26 @@
+import os
+
+from click.testing import CliRunner
+from mic.__main__ import credentials
 from mic.model_catalog_utils import get_api
 
 
+def test_configure():
+    runner = CliRunner()
+    model_catalog = os.environ['MODEL_CATALOG_PASSWORD']
+    result = runner.invoke(credentials,
+                           ["--server", "https://api.models.mint.isi.edu/v1.4.0",
+                            "--username", "mosorio@isi.edu",
+                            "--git_username", "mintbot",
+                            "--git_token", "asdfsafs",
+                            "--password", model_catalog,
+                            "--dockerhub_username", "a",
+                            "--name", "pedro",
+                            "--profile", "testing"])
+    print(result.stdout)
+    assert result.exit_code == 0
+
+
 def test_get_api():
-    get_api()
-    assert False
+    api = get_api(profile="testing")
+    assert api
