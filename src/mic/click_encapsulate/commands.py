ec68abd45a25b6d34445911ae1370e03625bdef5
Maximiliano Osorio <maxiosorio@gmail.com>
2020-06-19 14:38:00 -0700

    fix: continue/overwrite

--- src/mic/click_encapsulate/commands.py
+++ src/mic/click_encapsulate/commands.py
@@ -93,8 +93,8 @@ pip freeze > mic/docker/requirements.txt
              context_settings=dict(
                  ignore_unknown_options=True,
              ))
-@click.option('--c/--continue', 'c', default=None, help="add to the previous trace, don't replace it")
-@click.option('--o/--overwrite', 'o', default=None, help="overwrite the previous trace, don't add to it")
+@click.option('--continue', 'c', default=None, help="add to the previous trace, don't replace it")
+@click.option('--overwrite', 'o', default=None, help="overwrite the previous trace, don't add to it")
 @click.argument('command', nargs=-1, type=click.UNPROCESSED)
 def trace(command, c, o):
     """
