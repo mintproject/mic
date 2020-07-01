606d11936b8c7c6211a3ab0c54a4a19b35395ce9
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-01 09:16:02 -0700

    fix: add docker image option
    fix #161

--- src/mic/tests/test_commands.py
+++ src/mic/tests/test_commands.py
@@ -1,8 +1,10 @@
+import io
+import os
 import shutil
 from pathlib import Path
 
 from click.testing import CliRunner
-from mic.click_encapsulate.commands import add_parameters, DATATYPE_KEY
+from mic.click_encapsulate.commands import add_parameters, DATATYPE_KEY, start
 from mic.config_yaml import get_parameters
 
 RESOURCES = "resources"
@@ -35,6 +37,7 @@ def test_add_parameters_exit1(tmpdir):
         assert False
     assert result.exit_code == 1
 
+
 def test_add_parameters_exit0(tmpdir):
     runner = CliRunner()
     mic_file = Path(__file__).parent / RESOURCES / "mic_parameters.yaml"
@@ -53,7 +56,7 @@ def test_add_parameters_float(tmpdir):
     runner = CliRunner()
     mic_file = Path(__file__).parent / RESOURCES / "mic_parameters.yaml"
     mic_file = shutil.copy(mic_file, tmpdir / mic_file.name)
-    values = [1.0, -1.0, -.0, .0,  '1.0', '-1.0', '-.0', '.0', 0.0, '0.0']
+    values = [1.0, -1.0, -.0, .0, '1.0', '-1.0', '-.0', '.0', 0.0, '0.0']
     for index, value in enumerate(values):
         try:
             result = runner.invoke(add_parameters, ["-f", str(mic_file), "--name", index, "--value", value],
@@ -63,6 +66,7 @@ def test_add_parameters_float(tmpdir):
         assert get_parameters(mic_file)[index]["default_value"] == float(value)
         assert get_parameters(mic_file)[index][DATATYPE_KEY] == "float"
         assert result.exit_code == 0
+
 
 def test_add_parameters_integer(tmpdir):
     runner = CliRunner()
@@ -76,5 +80,21 @@ def test_add_parameters_integer(tmpdir):
         except Exception as e:
             assert False
         assert get_parameters(mic_file)[index]["default_value"] == int(value)
-        #assert get_parameters(mic_file)[index][DATATYPE_KEY] == "int"
+        # assert get_parameters(mic_file)[index][DATATYPE_KEY] == "int"
         assert result.exit_code == 0
+
+
+def test_start(tmp_path, monkeypatch):
+    runner = CliRunner()
+    os.chdir(tmp_path)
+    monkeypatch.setattr('sys.stdin', io.StringIO("\nexit\n"))
+    result = runner.invoke(start, ["--name", "test"], catch_exceptions=False)
+    assert result.exit_code == 0
+
+
+def test_start_image(tmp_path, monkeypatch):
+    runner = CliRunner()
+    os.chdir(tmp_path)
+    monkeypatch.setattr('sys.stdin', io.StringIO("\nexit\n"))
+    result = runner.invoke(start, ["--name", "test", "--image", "busybox"], catch_exceptions=False)
+    assert result.exit_code == 0
