c31b96099aa1151a811ee6b5ff55c6f8b7e72291
Maximiliano Osorio <maxiosorio@gmail.com>
2021-04-12 10:44:29 -0400

    add: test api
    related #324

---
+++ src/mic/tests/test_model_catalog_utils.py
@@ -0,0 +1,39 @@
+import os
+
+from click.testing import CliRunner
+from mic.__main__ import credentials
+from mic.model_catalog_utils import get_api
+
+
+def test_configure():
+    runner = CliRunner()
+    model_catalog_username = os.environ['MODEL_CATALOG_USERNAME_TEST']
+    model_catalog_password = os.environ['MODEL_CATALOG_PASSWORD_TEST']
+    result = runner.invoke(credentials, [
+        "--username", model_catalog_username,
+        "--git_username", "mintbot",
+        "--git_token", "asdfsafs",
+        "--password", model_catalog_password,
+        "--dockerhub_username", "a",
+        "--name", "pedro",
+        "--profile", "testing"
+    ])
+    assert result.exit_code == 0
+
+
+def test_get_api():
+    runner = CliRunner()
+    model_catalog_username = os.environ['MODEL_CATALOG_USERNAME_TEST']
+    model_catalog_password = os.environ['MODEL_CATALOG_PASSWORD_TEST']
+    result = runner.invoke(credentials, [
+        "--username", "mosorio@isi.edu",
+        "--git_username", "mintbot",
+        "--git_token", "asdfsafs",
+        "--password", model_catalog,
+        "--dockerhub_username", "a",
+        "--name", "pedro",
+        "--profile", "testing"
+    ])
+    assert result.exit_code == 0
+    api = get_api(profile="testing")
+    assert api
