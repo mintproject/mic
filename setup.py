f06d86dd7d1e6044f774a5fe94af3f9af0423b74
Maximiliano Osorio <maxiosorio@gmail.com>
2021-04-16 09:14:03 -0400

    Update model catalog dependency

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ install_requires = [
     "tabulate>=0.8.1",
     "Jinja2>=2.11.2",
     "PyYAML>=5.3.1",
-    "modelcatalog-api==7.0.0",
-    "dame-cli>=5.5.1",
+    "modelcatalog-api==7.1.0",
+    "dame-cli>=5.6.0",
     "pygit2>=1.2.1",
     "PyGithub>=1.43.5"
 ]
