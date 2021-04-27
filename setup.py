d5443b6851502160125310956ca2ba09795b3639
Maximiliano Osorio <maxiosorio@gmail.com>
2021-04-27 11:06:04 -0400

    fix: add dame cli as dep

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ install_requires = [
     "Jinja2>=2.11.2",
     "PyYAML>=5.3.1",
     "modelcatalog-api>=7.0.0",
+    "dame-cli>=5.5.1",
     "pygit2>=1.2.1",
     "PyGithub>=1.43.5",
     "ipython2cwl",
-    "validators"
 ]
 
 
